593740c9275c315bfd5018e2803b6c69d7fd87fd
theemadnes <alex.mattson@gmail.com>
2021-10-01 00:00:04 -0500

    basic CLI working

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # gke-keda-pubsub-test
 The repo name says it all - just testing a sample autoscaled app for job processing
+
+#### infra setup 
+
+set env vars
+```
+export PROJECT=$(gcloud config get-value project) # or your preferred project
+export GKE_REGION=us-central1 # or whatever you want to use 
+export PUBSUB_INGEST_TOPIC=injest-topic
+export PUBSUB_OUTPUT_TOPIC=output-topic
+export PUBSUB_INGEST_SUBSCRIPTION=injest-subscription
+export PUBSUB_OUTPUT_SUBSCRIPTION=output-subscription
+export WORKER_SERVICE_ACCOUNT=worker-sa
+export KEDA_SERVICE_ACCOUNT=keda-sa
+```
+
+create the GKE cluster
+```
+gcloud container clusters create keda-pubsub \
+    --machine-type=e2-standard-4 \
+    --num-nodes=1 \
+    --region ${GKE_REGION} \
+    --enable-stackdriver-kubernetes \
+    --enable-ip-alias \
+    --workload-pool=${PROJECT}.svc.id.goog \
+    --release-channel rapid
+```
+
+deploy KEDA
+```
+kubectl apply -f https://github.com/kedacore/keda/releases/download/v2.4.0/keda-2.4.0.yaml
+```
+
+create the pubsub resources
+```
+gcloud pubsub topics create $PUBSUB_INGEST_TOPIC
+gcloud pubsub topics create $PUBSUB_OUTPUT_TOPIC
+gcloud pubsub subscriptions create $PUBSUB_INGEST_SUBSCRIPTION \
+    --topic=${PUBSUB_INGEST_TOPIC} \
+    --ack-deadline=600
+gcloud pubsub subscriptions create $PUBSUB_OUTPUT_SUBSCRIPTION \
+    --topic=${PUBSUB_OUTPUT_TOPIC} \
+    --ack-deadline=60
+```
+
+#### create worker image 
+
+```
+
+```
+
+#### send test messages to the ingest topic 
+
+```
+for i in {1..20}
+do
+   gcloud pubsub topics publish $PUBSUB_INGEST_TOPIC \
+   --message=$RANDOM
+done
+```
---
+++ worker/app.py
@@ -0,0 +1,73 @@
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+import os
+
+# TODO(developer)
+project_id = os.environ.get('PROJECT')
+subscription_id = os.environ.get('PUBSUB_INGEST_SUBSCRIPTION')
+# Number of seconds the subscriber should listen for messages
+timeout = 60.0
+output_topic_id = os.environ.get('PUBSUB_OUTPUT_TOPIC')
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+publisher = pubsub_v1.PublisherClient()
+# The `topic_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/topics/{topic_id}`
+publish_topic_path = publisher.topic_path(project_id, output_topic_id)
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+
+    # determine if input value is prime number
+    # code sample from https://www.programiz.com/python-programming/examples/prime-number
+
+    print(message)
+    num = int(message.data)
+
+    # define a flag variable
+    flag = False
+
+    # prime numbers are greater than 1
+    if num > 1:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                # if factor is found, set flag to True
+                flag = True
+                # break out of loop
+                break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+        data = f"{num} is not a prime number"
+        data = data.encode("utf-8")
+        future = publisher.publish(publish_topic_path, data)
+        print(future.result())
+
+    else:
+        print(num, "is a prime number")
+        data = f"{num} is a prime number"
+        data = data.encode("utf-8")
+        future = publisher.publish(publish_topic_path, data)
+        print(future.result())
+
+    print(f"Received {message}.")
+
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done.
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result(timeout=timeout)
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
